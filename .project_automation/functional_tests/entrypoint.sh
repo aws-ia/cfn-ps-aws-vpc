928e4cfc7fa53b5a4cd4981b95a29753fc4d3ab7
IA Automator <dev@null>
2023-04-18 20:19:07 +0000

    (automated) Updates from project type

--- .project_automation/functional_tests/entrypoint.sh
+++ .project_automation/functional_tests/entrypoint.sh
@@ -8,19 +8,30 @@ PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
 ##----------------------------------------------------
 ## Download taskcat overrides from AWS Secrets Manager
 ## ---------------------------------------------------
-## Create a 'secret' of type plaintext in AWS Secrets Manager 
+## Create a 'secret' of type plaintext in AWS Secrets Manager
 ## and add taskcat override file contents to it.
 ## Provide secret name to 'secret_name' parameter below and
 ## the AWS region where you secret is stored to 'aws_region'.
 
 # set defaults
-secret_name="taskcat_overrides"
+secret_name=$(cat .taskcat.yml | yq -r '.project|.name')
+secret_name=${secret_name}-override
 secret_region="us-east-1"
-# retrieve the secret value as a JSON string
+# If overrides secret exists, retrieve the secret value as a JSON string
+set +e
 overrides=$(aws secretsmanager get-secret-value --secret-id $secret_name --query SecretString --output text --region $secret_region)
 # convert the JSON string to YAML and save it to a file
-echo "$overrides" > .taskcat_overrides.yml
+if [ "#?" -eq 0 ]; then
+  echo "$overrides" > .taskcat_overrides.yml
+fi
+set -e
 ##----------------------------------------------------
+
+# set taskcat general config
+cat << EOF > ~/.taskcat.yml
+general:
+  s3_regional_buckets: true
+EOF
 
 # Run taskcat tests
 taskcat test run
